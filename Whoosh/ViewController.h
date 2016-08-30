7cb67c1204b1bb77103ffe7c8e32e01fba9a4f7a
Dan Esrey <danesrey@dans-macbook-air.local>
2016-08-30 16:06:49 -0400

    Initial Commit

---
+++ Whoosh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Whoosh
+//
+//  Created by Dan Esrey on 2016/30/08.
+//  Copyright © 2016 Dan Esrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Whoosh/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Whoosh
+//
+//  Created by Dan Esrey on 2016/30/08.
+//  Copyright © 2016 Dan Esrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
